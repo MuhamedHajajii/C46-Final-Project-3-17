e2fea2d43ed92ca03882ac6b3ff3b643e4de0ed1
Muhamedhajaji <muhamed.hajajii@shezlong.com>
2026-03-25 00:51:20 +0200

    dist

--- src/app/core/interceptors/environment.interceptor.ts
+++ src/app/core/interceptors/environment.interceptor.ts
@@ -1,11 +1,11 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 export const environmentInterceptor: HttpInterceptorFn = (req, next) => {
   // baseUrl
   console.log(environment.baseUrl);
   console.log(req.url);
-
+  console.log(environment.appUrl);
   return next(
     req.clone({
       url: `${environment.baseUrl}${req.url}`,
--- src/app/features/checkout/services/checkout.service.ts
+++ src/app/features/checkout/services/checkout.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { baseHttp } from '../../../core/services/base-http.service';
 import { HttpParams } from '@angular/common/http';
-import { environment } from '../../../../environments/environment.development';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
